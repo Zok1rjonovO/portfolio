99aa234ca5e2e7dd8a747c376bcec20dddff3aab
Olimjon Zokirjonov <olimjonzokirjonov@Olimjons-MacBook-Air.local>
2023-07-03 20:23:55 +0500

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-
-import Logo from "../assets/logo.svg";
-
+import { fadeIn } from "../variants";
+import { motion } from "framer-motion";
 const Header = () => {
   return (
     <header className=" py-8">
       <div className="container mx-auto">
         <div className="flex flex-row justify-between items-center mt-8 text-[24px] lg:text-[32px] font-secondary font-semibold uppercase leading-[1] ">
           <a href="#" className="flex flex-col gap-3 text-white mr-4">
-            <h1 className="text-accent">Zokrijonov</h1>
-            <h1>Olimjon</h1>
+          <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+            >
+              <h3 className="text-accent">Zokrijonov</h3>
+              <h3>Olimjon</h3>
+            </motion.h1>
           </a>
+          <motion.button
+          variants={fadeIn("up", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}>
           <button className="btn btn-sm">work with me</button>
+          </motion.button>
         </div>
       </div>
     </header>
